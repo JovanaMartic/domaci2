8ab63d29ae5f214496a42d7711fe28ba8f6530c4
JovanaMartic <jovanamartic1@gmail.com>
2022-12-15 20:10:48 +0100

    migracije

--- prevoz/database/migrations/2022_12_15_190801_service_id_dodavanje.php
+++ prevoz/database/migrations/2022_12_15_190801_service_id_dodavanje.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration
+class AddColumnServiceIdToReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->integer('rate');
-            $table->text('body');
-            $table->timestamps();
-           // $table->timestamps('published');
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->foreignId("service_id");
         });
     }
 
@@ -29,6 +25,8 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reviews');
+        Schema::table('reviews', function (Blueprint $table) {
+          //  $table->dropForeign(["service_id"]);
+        });
     }
-}
+};
--- prevoz/database/migrations/2022_12_15_190900_price_dodavanje.php
+++ prevoz/database/migrations/2022_12_15_190900_price_dodavanje.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration
+class AddColumnPriceToServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->integer('rate');
-            $table->text('body');
-            $table->timestamps();
-           // $table->timestamps('published');
+        Schema::table('services', function (Blueprint $table) {
+            $table->integer('price');
         });
     }
 
@@ -29,6 +25,8 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reviews');
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn('price');
+        });
     }
-}
+};
